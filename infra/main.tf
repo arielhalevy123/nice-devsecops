09f845f6e65d7c624924280fee7f22331ba82b7a
arielhalevy123 <130355480+arielhalevy123@users.noreply.github.com>
2025-09-07 17:48:43 +0300

    change main.tf file added the first block

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "devsecops-scan-reports-ariel"
+    key    = "tofu/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
